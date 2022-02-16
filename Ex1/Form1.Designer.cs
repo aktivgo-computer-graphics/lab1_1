d8967019531378db1c426ec5c9df1f15b51ddb81
aktivgo <AktivGo01062001@gmail.com>
2022-02-16 09:53:38 +0300

    create graph

--- Ex1/Form1.Designer.cs
+++ Ex1/Form1.Designer.cs
@@ -29,7 +29,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.paintButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // paintButton
+            // 
+            this.paintButton.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.paintButton.Location = new System.Drawing.Point(650, 12);
+            this.paintButton.Name = "paintButton";
+            this.paintButton.Size = new System.Drawing.Size(120, 50);
+            this.paintButton.TabIndex = 0;
+            this.paintButton.Text = "Построить";
+            this.paintButton.UseVisualStyleBackColor = true;
+            this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
             // 
             // Form1
             // 
@@ -37,12 +50,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.paintButton);
             this.Name = "Form1";
             this.Text = "График";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button paintButton;
 
         #endregion
     }
--- Ex1/Form1.cs
+++ Ex1/Form1.cs
@@ -17,32 +17,63 @@ namespace Ex1
 
         public Form1()
         {
-            Graph = CreateGraphics();
-            MyPen = new Pen(Color.Magenta);
             InitializeComponent();
+            Graph = CreateGraphics();
+            MyPen = new Pen(Color.Black);
         }
         
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void paintButton_Click(object sender, EventArgs e)
         {
-            int x0 = ClientSize.Width / 2;
-            int y0 = ClientSize.Height / 2;
+            const int k = 90;
             const double xMin = -5;
             const double xMax = 5;
             const double step = 0.01;
-            const double k = 5.5;
+            
+            Graph.Clear(Color.White);
+            PaintOXY(k);
+            
+            int x0 = ClientSize.Width / 2;
+            int y0 = ClientSize.Height / 2;
             double x = xMin;
-            double y = - x * x + 3;
+            double y = - x * x + 4;
             int x1 = (int)(x0 + x * k);
             int y1 = (int)(y0 - y * k);
             while (x < xMax)
             {
                 x = x + step;
-                y = - x * x + 3;
+                y = - x * x + 4;
                 int x2 = (int)(x0 + x * k);
                 int y2 = (int)(y0 - y * k);
                 Graph.DrawLine(MyPen, x1, y1, x2, y2);
                 x1 = x2;
                 y1 = y2;
+            }
+        }
+        
+        private void PaintOXY(int k)
+        {
+            int xMiddle = ClientSize.Width / 2;
+            int yMiddle = ClientSize.Height / 2;
+            Graph.DrawLine(MyPen, 0, yMiddle, ClientSize.Width, yMiddle);
+            Graph.DrawLine(MyPen, xMiddle, 0, xMiddle, ClientSize.Height);
+            for (int x = xMiddle + k; x < ClientSize.Width; x += k)
+            {
+                Graph.DrawLine(MyPen, x, yMiddle - 5, x, yMiddle + 5);
+                Graph.DrawString(((x - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x - 5, yMiddle + 15);
+            }
+            for (int x = xMiddle - k; x > 0; x -= k)
+            {
+                Graph.DrawLine(MyPen, x, yMiddle - 5, x, yMiddle + 5);
+                Graph.DrawString(((x - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x - 7, yMiddle + 15);
+            }
+            for (int y = yMiddle + k; y < ClientSize.Width; y += k)
+            {
+                Graph.DrawLine(MyPen, xMiddle - 5, y, xMiddle + 5, y);
+                Graph.DrawString(((y - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, xMiddle + 7, y + 7);
+            }
+            for (int y = yMiddle - k; y > 0; y -= k)
+            {
+                Graph.DrawLine(MyPen, xMiddle - 5, y, xMiddle + 5, y);
             }
         }
 
