fba5ebc3f71039cb831d7247e3c017fd6a2f1eff
aktivgo <AktivGo01062001@gmail.com>
2022-02-20 14:08:41 +0300

    complete task

--- Ex1/Form1.Designer.cs
+++ Ex1/Form1.Designer.cs
@@ -30,19 +30,29 @@
         private void InitializeComponent()
         {
             this.paintButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // paintButton
             // 
-            this.paintButton.Anchor = ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-            this.paintButton.Location = new System.Drawing.Point(650, 12);
+            this.paintButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.paintButton.Location = new System.Drawing.Point(12, 85);
             this.paintButton.Name = "paintButton";
-            this.paintButton.Size = new System.Drawing.Size(120, 50);
+            this.paintButton.Size = new System.Drawing.Size(220, 50);
             this.paintButton.TabIndex = 0;
             this.paintButton.Text = "Построить";
             this.paintButton.UseVisualStyleBackColor = true;
             this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 70);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "y = Sin(3x + 1)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -50,6 +60,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.paintButton);
             this.Name = "Form1";
             this.Text = "График";
@@ -57,6 +68,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label1;
 
         private System.Windows.Forms.Button paintButton;
 
--- Ex1/Form1.cs
+++ Ex1/Form1.cs
@@ -14,34 +14,36 @@ namespace Ex1
     {
         private Graphics Graph;
         private Pen MyPen;
+        private int PiMode; // 0 - выкл, 1 - вкл, числа, 2 - вкл, pi числа
 
         public Form1()
         {
             InitializeComponent();
             Graph = CreateGraphics();
             MyPen = new Pen(Color.Black);
+            PiMode = 2;
         }
         
         private void paintButton_Click(object sender, EventArgs e)
         {
-            const int k = 90;
-            const double xMin = -5;
-            const double xMax = 5;
+            const int k = 200;
+            const double xMin = -2;
+            const double xMax = 2;
             const double step = 0.01;
             
             Graph.Clear(Color.White);
-            PaintOXY(k);
+            PaintOxy(k);
             
             int x0 = ClientSize.Width / 2;
             int y0 = ClientSize.Height / 2;
             double x = xMin;
-            double y = - x * x + 4;
+            double y = Math.Sin(3 * x + 1);
             int x1 = (int)(x0 + x * k);
             int y1 = (int)(y0 - y * k);
             while (x < xMax)
             {
-                x = x + step;
-                y = - x * x + 4;
+                x += step;
+                y = Math.Sin(3 * x + 1);
                 int x2 = (int)(x0 + x * k);
                 int y2 = (int)(y0 - y * k);
                 Graph.DrawLine(MyPen, x1, y1, x2, y2);
@@ -50,31 +52,83 @@ namespace Ex1
             }
         }
         
-        private void PaintOXY(int k)
+        private void PaintOxy(int k)
         {
             int xMiddle = ClientSize.Width / 2;
             int yMiddle = ClientSize.Height / 2;
+            
             Graph.DrawLine(MyPen, 0, yMiddle, ClientSize.Width, yMiddle);
             Graph.DrawLine(MyPen, xMiddle, 0, xMiddle, ClientSize.Height);
-            for (int x = xMiddle + k; x < ClientSize.Width; x += k)
+            Graph.DrawString("0", new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, xMiddle + 7, yMiddle + 7);
+            
+            for (int x = xMiddle + k, i = 0; x < ClientSize.Width; x += k, i++)
             {
                 Graph.DrawLine(MyPen, x, yMiddle - 5, x, yMiddle + 5);
-                Graph.DrawString(((x - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x - 5, yMiddle + 15);
+                ChooseNumAndDraw(i, false, xMiddle, k, x + 5, yMiddle + 15);
             }
-            for (int x = xMiddle - k; x > 0; x -= k)
+            for (int x = xMiddle - k, i = 0; x > 0; x -= k, i++)
             {
                 Graph.DrawLine(MyPen, x, yMiddle - 5, x, yMiddle + 5);
-                Graph.DrawString(((x - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x - 7, yMiddle + 15);
+                ChooseNumAndDraw(i, true, xMiddle, k, x - 7, yMiddle + 15);
+                //Graph.DrawString(((x - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x - 7, yMiddle + 15);
             }
             for (int y = yMiddle + k; y < ClientSize.Width; y += k)
             {
                 Graph.DrawLine(MyPen, xMiddle - 5, y, xMiddle + 5, y);
-                Graph.DrawString(((y - xMiddle) / k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, xMiddle + 7, y + 7);
+                Graph.DrawString(((y - yMiddle) / -k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, xMiddle + 15, y - 7);
             }
             for (int y = yMiddle - k; y > 0; y -= k)
             {
                 Graph.DrawLine(MyPen, xMiddle - 5, y, xMiddle + 5, y);
+                Graph.DrawString(((y - yMiddle) / -k).ToString(), new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, xMiddle + 15, y - 7);
             }
+        }
+
+        private void ChooseNumAndDraw(int i, bool minus, int xMiddle, int k, int x, int y)
+        {
+            string numStr = "";
+            double num = 0;
+            switch (i)
+            {
+                case 0:
+                    numStr = minus ? "-π/2" : "π/2";
+                    num = minus ? -3.14 : 3.14;
+                    break;
+                case 1:
+                    numStr = minus ? "-π" : "π";
+                    num = minus ? -6.28 : 6.28;
+                    break;
+                case 2:
+                    numStr = minus ? "-3π/2" : "3π/2";
+                    num = minus ? -9.42 : 9.42;
+                    break;
+                case 3:
+                    numStr = minus ? "-2π" : "2π";
+                    num = minus ? -12.56 : 12.56;
+                    break;
+                case 4:
+                    numStr = minus ? "-5π/2" : "5π/2";
+                    num = minus ? -15.7 : 15.7;
+                    break;
+            }
+            
+            switch (PiMode)
+            {
+                case 0:
+                    DrawString(((x - xMiddle) / k).ToString(), x, y);
+                    break;
+                case 1:
+                    DrawString(num.ToString(), x, y);
+                    break;
+                case 2:
+                    DrawString(numStr, x, y);
+                    break;
+            }
+        }
+
+        private void DrawString(string num, int x, int y)
+        {
+            Graph.DrawString(num, new Font(FontFamily.GenericSansSerif, 10), Brushes.Black, x, y);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
